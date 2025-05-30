4c2d025d569c8712156b1a954046303b8fcff5bf
JosiasPires <104743492+JosiasPires@users.noreply.github.com>
2025-05-29 22:45:36 -0300

    feat: add script to fetch space news for each solar system body

---
+++ src/api/news.js
@@ -0,0 +1,20 @@
+import { solarSystem } from "../data/solar-system.js";
+import fs from "fs/promises";
+import path from "path";
+
+const __dirname = import.meta.dirname;
+
+const API_KEY = process.env.API_KEY;
+const news = {};
+
+for (const { name } of solarSystem) {
+  const res = await fetch(
+    `https://api.currentsapi.services/v1/search?category=space&domain=space.com&page_size=3&keywords=${name}&apiKey=${API_KEY}`,
+  );
+  const data = await res.json();
+  news[name] = data.news;
+}
+
+const filePath = path.join(__dirname, "../data/news.json");
+
+await fs.writeFile(filePath, JSON.stringify(news));
