1c46d6fa502939a185a9d52d5f4a5159b29134b2
JosiasPires <104743492+JosiasPires@users.noreply.github.com>
2025-05-31 04:27:45 -0300

    feat: add progress logging while fetching news data

--- src/api/news.js
+++ src/api/news.js
@@ -8,14 +8,23 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const API_KEY = process.env.API_KEY;
 const news = {};
 
-for (const { name } of solarSystem) {
+for (let i = 0; i < solarSystem.length; i++) {
+  const { name } = solarSystem[i];
+
+  console.log(
+    `${i + 1}/${solarSystem.length} - Fetching news about ${name}...`,
+  );
+
   const res = await fetch(
     `https://api.currentsapi.services/v1/search?category=space&domain=space.com&page_size=3&keywords=${name}&apiKey=${API_KEY}`,
   );
+
   const data = await res.json();
   news[name] = data.news;
 }
 
 const filePath = path.join(__dirname, "../data/news.json");
 
+console.log("Writing responses to news.json file...");
 await fs.writeFile(filePath, JSON.stringify(news));
+console.log("Done");
