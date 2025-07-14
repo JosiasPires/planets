4d830b80be5accc545f5e3c0530a1c18e74dbbae
JosiasPires <104743492+JosiasPires@users.noreply.github.com>
2025-07-14 01:59:52 -0300

    feat: change selected planet with info

--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
-import { loadPlanets } from "./modules/planets";
+import { solarSystem } from "./data/solar-system";
+import { loadInfo, loadPlanets, transformPlanetsY } from "./modules/planets";
 import "./style.css";
 
+let selected = 0;
 loadPlanets();
+loadInfo(selected);
+
+document.querySelector('#previous').addEventListener('click', () => {
+    if (selected > 0) {
+        transformPlanetsY('previous');
+        selected = selected - 1;
+    }
+    loadInfo(selected);
+})
+document.querySelector('#next').addEventListener('click', () => {
+    if (selected < solarSystem.length - 1) {
+        transformPlanetsY('next');
+        selected = selected + 1
+    }
+    else {
+        transformPlanetsY('reset');
+        selected = 0;
+    }
+    loadInfo(selected);
+})
--- src/modules/planets.js
+++ src/modules/planets.js
@@ -7,21 +7,32 @@ export function loadPlanets() {
     for (let star of solarSystem) {
         const starNode = document.createElement('img');
         starNode.src = `${star.name.toLowerCase()}.png`;
-        starNode.className = 'min-h-full grayscale-75';
+        starNode.className = 'min-h-full';
         starNode.style.order = order;
         planetList.appendChild(starNode);
-        if (order == 0) {
-            starNode.id = 'selected';
-            loadInfo();
-        }
         order++;
     }
 }
 
-export function loadInfo() {
-    document.querySelector('#name').textContent = solarSystem[0].name;
-    document.querySelector('#description').textContent = solarSystem[0].description;
-    document.querySelector('#radius').textContent = solarSystem[0].radius_km + 'km';
-    document.querySelector('#mass').textContent = solarSystem[0].mass_kg + 'kg';
-    document.querySelector('#distance').textContent = solarSystem[0].distance_from_sun_km + 'km';
+let y = 0
+const height = document.querySelector('#planetList').offsetHeight;
+export function transformPlanetsY(option) {
+    if (option == 'next') {
+        y -= height; 
+    }
+    else if (option == 'reset') {
+        y = 0;
+    }
+    else if (option == 'previous') {
+        y += height; 
+    }
+    planetList.style.transform = `translateY(${y}px)`;
+}
+
+export function loadInfo(order) {
+    document.querySelector('#name').textContent = solarSystem[order].name;
+    document.querySelector('#description').textContent = solarSystem[order].description;
+    document.querySelector('#radius').textContent = solarSystem[order].radius_km + 'km';
+    document.querySelector('#mass').textContent = solarSystem[order].mass_kg + 'kg';
+    document.querySelector('#distance').textContent = solarSystem[order].distance_from_sun_km + 'km';
 }
