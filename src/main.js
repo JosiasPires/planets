2a8269f4048c4cff87081539dd3bf6f534d56e8a
JosiasPires <104743492+JosiasPires@users.noreply.github.com>
2025-07-14 03:25:47 -0300

    chore: format code with Prettier

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { solarSystem } from "./data/solar-system";
-import { loadInfo, loadNews, loadPlanets, transformPlanetsY } from "./modules/planets";
+import {
+  loadInfo,
+  loadNews,
+  loadPlanets,
+  transformPlanetsY,
+} from "./modules/planets";
 import "./style.css";
 
 let selected = 0;
@@ -7,23 +12,22 @@ loadPlanets();
 loadInfo(selected);
 loadNews(selected);
 
-document.querySelector('#previous').addEventListener('click', () => {
-    if (selected > 0) {
-        transformPlanetsY('previous');
-        selected = selected - 1;
-    }
-    loadInfo(selected);
-    loadNews(selected);
-})
-document.querySelector('#next').addEventListener('click', () => {
-    if (selected < solarSystem.length - 1) {
-        transformPlanetsY('next');
-        selected = selected + 1
-    }
-    else {
-        transformPlanetsY('reset');
-        selected = 0;
-    }
-    loadInfo(selected);
-    loadNews(selected);
-})
+document.querySelector("#previous").addEventListener("click", () => {
+  if (selected > 0) {
+    transformPlanetsY("previous");
+    selected = selected - 1;
+  }
+  loadInfo(selected);
+  loadNews(selected);
+});
+document.querySelector("#next").addEventListener("click", () => {
+  if (selected < solarSystem.length - 1) {
+    transformPlanetsY("next");
+    selected = selected + 1;
+  } else {
+    transformPlanetsY("reset");
+    selected = 0;
+  }
+  loadInfo(selected);
+  loadNews(selected);
+});
--- src/modules/planets.js
+++ src/modules/planets.js
@@ -4,47 +4,50 @@ import news from "../data/news.json";
 const planetList = document.querySelector("#planetList");
 
 export function loadPlanets() {
-    let order = 0;
-    for (let star of solarSystem) {
-        const starNode = document.createElement('img');
-        starNode.src = `${star.name.toLowerCase()}.png`;
-        starNode.className = 'min-h-full';
-        starNode.style.order = order;
-        planetList.appendChild(starNode);
-        order++;
-    }
+  let order = 0;
+  for (let star of solarSystem) {
+    const starNode = document.createElement("img");
+    starNode.src = `${star.name.toLowerCase()}.png`;
+    starNode.className = "min-h-full";
+    starNode.style.order = order;
+    planetList.appendChild(starNode);
+    order++;
+  }
 }
 
-let y = 0
-const height = document.querySelector('#planetList').offsetHeight;
+let y = 0;
+const height = document.querySelector("#planetList").offsetHeight;
 export function transformPlanetsY(option) {
-    if (option == 'next') {
-        y -= height; 
-    }
-    else if (option == 'reset') {
-        y = 0;
-    }
-    else if (option == 'previous') {
-        y += height; 
-    }
-    planetList.style.transform = `translateY(${y}px)`;
+  if (option == "next") {
+    y -= height;
+  } else if (option == "reset") {
+    y = 0;
+  } else if (option == "previous") {
+    y += height;
+  }
+  planetList.style.transform = `translateY(${y}px)`;
 }
 
 export function loadInfo(order) {
-    document.querySelector('#name').textContent = solarSystem[order].name;
-    document.querySelector('#description').textContent = solarSystem[order].description;
-    document.querySelector('#radius').textContent = solarSystem[order].radius_km + 'km';
-    document.querySelector('#mass').textContent = solarSystem[order].mass_kg + 'kg';
-    document.querySelector('#distance').textContent = solarSystem[order].distance_from_sun_km + 'km';
+  document.querySelector("#name").textContent = solarSystem[order].name;
+  document.querySelector("#description").textContent =
+    solarSystem[order].description;
+  document.querySelector("#radius").textContent =
+    solarSystem[order].radius_km + "km";
+  document.querySelector("#mass").textContent =
+    solarSystem[order].mass_kg + "kg";
+  document.querySelector("#distance").textContent =
+    solarSystem[order].distance_from_sun_km + "km";
 }
 
 export function loadNews(order) {
-    const newsPlanet = news[solarSystem[order].name];
-    console.log(newsPlanet);
-    for (let i = 0; i < 3; i++) {
-        document.querySelector(`#news${i}`).href = newsPlanet[i].url;
-        document.querySelector(`#newsImage${i}`).src = newsPlanet[i].image;
-        document.querySelector(`#newsTitle${i}`).textContent = newsPlanet[i].title;
-        document.querySelector(`#newsDesc${i}`).textContent = newsPlanet[i].description;
-    }
+  const newsPlanet = news[solarSystem[order].name];
+  console.log(newsPlanet);
+  for (let i = 0; i < 3; i++) {
+    document.querySelector(`#news${i}`).href = newsPlanet[i].url;
+    document.querySelector(`#newsImage${i}`).src = newsPlanet[i].image;
+    document.querySelector(`#newsTitle${i}`).textContent = newsPlanet[i].title;
+    document.querySelector(`#newsDesc${i}`).textContent =
+      newsPlanet[i].description;
+  }
 }
