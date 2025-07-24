394c4deff2118d44455169d3df0445252e502d86
JosiasPires <104743492+JosiasPires@users.noreply.github.com>
2025-07-24 01:42:36 -0300

    feat: retry fetching news on server error until successful

--- src/api/news.js
+++ src/api/news.js
@@ -13,6 +13,23 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const API_KEY = process.env.API_KEY;
 const news = {};
 
+async function fetchNews(topic) {
+  while (true) {
+    const response = await fetch(
+      `https://api.currentsapi.services/v1/search?category=space&domain=space.com&page_size=3&start_date=${startDate}&keywords=${topic}&apiKey=${API_KEY}`,
+    );
+    const data = await response.json();
+
+    if (data.status !== "500") {
+      console.log("News fetched successfully!");
+      return data.news;
+    } else {
+      console.log("error, trying again");
+    }
+    await new Promise((res) => setTimeout(res, 2000));
+  }
+}
+
 for (let i = 0; i < solarSystem.length; i++) {
   const { name } = solarSystem[i];
 
@@ -20,12 +37,7 @@ for (let i = 0; i < solarSystem.length; i++) {
     `${i + 1}/${solarSystem.length} - Fetching news about ${name}...`,
   );
 
-  const res = await fetch(
-    `https://api.currentsapi.services/v1/search?category=space&domain=space.com&page_size=3&start_date=${startDate}&keywords=${name}&apiKey=${API_KEY}`,
-  );
-
-  const data = await res.json();
-  news[name] = data.news;
+  news[name] = await fetchNews(name);
 }
 
 const filePath = path.join(__dirname, "../data/news.json");
